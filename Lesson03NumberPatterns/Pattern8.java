f927e3b4d383150afe5bb911c3093a2a2b3e90a4
Dilyan Simov <115150533+d-simov@users.noreply.github.com>
2022-12-18 17:17:35 +0200

    Add files via upload

---
+++ Lesson03NumberPatterns/Pattern8.java
@@ -0,0 +1,19 @@
+package Lesson03NumberPatterns;
+
+public class Pattern8 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == i || j == 5 - 1 - i){
+                    System.out.print(1 + " ");
+                }
+                else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
