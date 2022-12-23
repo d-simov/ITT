ab6883389a01d63fb246b888720df6e0e93296d6
Dilyan Simov <115150533+d-simov@users.noreply.github.com>
2022-12-23 08:50:23 +0200

    Add files via upload

---
+++ Lesson04NestedLoopsFigures/Task1.java
@@ -0,0 +1,20 @@
+package Lesson04NestedLoopsFiguresFile;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
