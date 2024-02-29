a2b5fb91a2a89cf29601fc57e89cafa5d9cae96c
Dilyan Simov <simov.d.t@gmail.com>
2024-02-29 23:29:26 +0200

    Fixed bugs in the recursive decision of the ches task

--- _Interesting_Tasks/ChessRecursive.java
+++ _Interesting_Tasks/ChessRecursive.java
@@ -1,86 +1,90 @@
+package Lesson08StringsAndMethods;
+
 public class ChessRecursive {
     /*
     Шахматна дъска с царица - замръзнала. На няколко позиции има свои пешки, на една позиция има противников кон.
     Ще може ли някога конят, след безкрайни последователни ходове да вземе царицата?
      */
     public static void main(String[] args) {
+
         char[][] chessBoard = {
-                {'_','_','_','_','_','_','_','_'},
-                {'p','Q','_','p','_','_','_','_'},
-                {'_','p','_','_','_','_','p','_'},
-                {'_','_','p','_','p','_','_','_'},
-                {'_','p','_','_','_','_','_','_'},
-                {'_','_','_','_','_','_','_','p'},
-                {'_','_','_','_','_','K','_','_'},
-                {'_','_','_','_','_','_','_','_'}
+                {'_', '_', '_', '_', '_', '_', '_', '_'},
+                {'p', 'Q', '_', 'p', '_', '_', '_', '_'},
+                {'_', 'p', '_', '_', '_', '_', 'p', '_'},
+                {'_', '_', '_', '_', 'p', '_', 'p', '_'},
+                {'_', 'p', '_', 'p', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_', 'X', '_', '_'},
+                {'_', '_', '_', '_', '_', '_', '_', 'p'},
+                {'_', '_', '_', '_', '_', '_', 'p', '_'}
         };
 
-        // define chessBoard
-        //      -> give random Q position
-        //      -> give random K position -> different from Q position
-        //      -> give 8 random 'p' positions, different from Q and K
         chessBoard[1][1] = 'Q';
-        chessBoard[6][5] = 'K';
-
-        printBoard(chessBoard);
-        makePossibleKnightMoves(chessBoard, 6, 5, false, 0);
 
 
-        // if position Q -> END -> You WIN
-        // else -> mark new position as K
-        // REPEAT
-        // if all possible moves are not valid - You FAIL
+        if (moveKnight(chessBoard, 5, 5)) {
+            System.out.println("Success!");
+        }
+        else {
+            System.out.println("Failure.");
+        }
     }
-
     private static void printBoard(char[][] chessBoard) {
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[i].length; j++) {
-                System.out.print(chessBoard[i][j] + ", ");
+        for (char[] chars : chessBoard) {
+            for (char aChar : chars) {
+                System.out.print(aChar + ", ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    private static void makePossibleKnightMoves(char[][] chessBoard, int i, int j, boolean queenIsTaken, int unsuccessfulMoves) {
-        moveKnight(chessBoard, i-2, j+1, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i-1, j+2, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i+1, j+2, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i+2, j+1, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i+2, j-1, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i+1, j-2, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i-1, j-2, false, unsuccessfulMoves);
-        moveKnight(chessBoard, i-2, j-1, false, unsuccessfulMoves);
+    private static boolean moveKnight(char[][] chessBoard, int i, int j) {
+        boolean isQueenTaken = false;
         printBoard(chessBoard);
-        if (queenIsTaken) {
-            System.out.println("YOU WIN!");
-            return;
+        if (!outOfBound(i, j)) {
+            if (chessBoard[i][j] == 'Q') {
+                System.out.println("You successfully took over the enemy Queen!");
+                isQueenTaken = true;
+            }
         }
-        if (unsuccessfulMoves >= 8) {
-            System.out.println("The Queen can not be taken!");
-            return;
+        chessBoard[i][j] = 'K';
+        if (!isQueenTaken && validMove(chessBoard, i - 2, j + 1)) {
+            isQueenTaken = moveKnight(chessBoard, i - 2, j + 1);
         }
+        if (!isQueenTaken && validMove(chessBoard, i - 1, j + 2)) {
+            isQueenTaken = moveKnight(chessBoard, i - 1, j + 2);
+        }
+        if (!isQueenTaken && validMove(chessBoard,i + 1, j + 2)) {
+            isQueenTaken = moveKnight(chessBoard, i + 1, j + 2);
+        }
+        if (!isQueenTaken && validMove(chessBoard,i + 2, j + 1)) {
+            isQueenTaken = moveKnight(chessBoard, i + 2, j + 1);
+        }
+        if (!isQueenTaken && validMove(chessBoard, i + 2, j - 1)) {
+            isQueenTaken = moveKnight(chessBoard, i + 2, j - 1);
+        }
+        if (!isQueenTaken && validMove(chessBoard, i + 1, j - 2)) {
+            isQueenTaken = moveKnight(chessBoard, i + 1, j - 2);
+        }
+        if (!isQueenTaken && validMove(chessBoard, i - 1, j - 2)) {
+            isQueenTaken = moveKnight(chessBoard, i - 1, j - 2);
+        }
+        if (!isQueenTaken && validMove(chessBoard, i - 2, j - 1)) {
+            isQueenTaken = moveKnight(chessBoard, i - 2, j - 1);
+        }
+
+        if (!isQueenTaken) {
+            chessBoard[i][j] = '_';
+        }
+        return isQueenTaken;
 
     }
 
-    private static void moveKnight(char[][] chessBoard, int i, int j, boolean queenIsTaken, int unsuccessfulMoves) {
-        if(validMove(chessBoard, i, j)) {
-            if(chessBoard[i][j] == 'Q'){
-                System.out.println("You successfully took over the enemy Queen!");
-                queenIsTaken = true;
-                return;
-            }
-            else {
-                chessBoard[i][j] = 'K';
-                makePossibleKnightMoves(chessBoard, i, j, false, 0);
-            }
-        }
-        else {
-            ++unsuccessfulMoves;
-        }
+    private static boolean outOfBound(int i, int j) {
+        return i < 0 || i >= 8 || j < 0 || j >= 8;
     }
 
     private static boolean validMove(char[][] chessBoard, int i, int j) {
-        return (((i >= 0 && i < 8) && (j >= 0 && j < 8)) && chessBoard[i][j] != 'p' && chessBoard[i][j] != 'K');
+        return !outOfBound(i, j) && chessBoard[i][j] != 'p' && chessBoard[i][j] != 'K';
     }
 }
